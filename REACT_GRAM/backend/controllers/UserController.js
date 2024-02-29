91d7f504ea5a4fff1f3d22056989b115e0972701
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2024-02-28 23:53:13 -0300

    .

---
+++ REACT_GRAM/backend/controllers/UserController.js
@@ -0,0 +1,59 @@
+const User = require("../models/User")
+
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+const jwtSecret = process.env.JWT_SECRET
+
+// Generate user token
+const generateToken = (id) => {
+    return jwt.sign({ id }, jwtSecret, {
+        expiresIn: "7d"
+    })
+}
+
+// Register user and sign in
+const register = async (req, res) => {
+   
+    const {name, email, password} = req.body
+
+    //check if user exists
+    const user = await User.findOne({email}) //findOne - encontre um ...
+
+    if(user){
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.getSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // If user was created successfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+}
+
+
+// Sign user in
+const login = (req, res) => {
+    res.send("Login")
+}
+
+module.exports = {
+    register, 
+    login
+}
---
+++ REACT_GRAM/backend/routes/UserRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+// Controller
+const { register, login } = require("../controllers/UserController")
+
+// Middlewares
+const validate = require('../middlewares/handleValidation')
+const { userCreateValidation , loginValidation } = require("../middlewares/userValidations")
+
+// Router
+router.post("/register" , userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
+
+
+module.exports = router
