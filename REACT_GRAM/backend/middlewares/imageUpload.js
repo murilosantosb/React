10aa0d575743fb7b9d2ba6a7d2e29c9f274a6558
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2024-03-04 13:01:42 -0300

    .

---
+++ REACT_GRAM/backend/middlewares/imageUpload.js
@@ -0,0 +1,22 @@
+const multer = require("multer")
+const path = require("path")
+
+// Destination to store image
+const imageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = ""
+
+        if(req.baseUrl.includes("users")) {
+            folder = "users"
+        }else if(req.baseUrl.includes("photos")) {
+            folder = "photos"
+        }
+
+        cb(null, `uploads/${folder}/`)
+    },
+    filename: (req, file, cb) => {
+
+        cb(null, Date.now() + path.extname(file.originalname))
+
+    }
+})
